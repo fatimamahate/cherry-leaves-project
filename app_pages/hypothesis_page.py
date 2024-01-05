802d7952ce79bf19efef6cae6d690dd6a0c822bc
fatimamahate <>
2024-01-05 05:32:44 +0000

    edit hypothesis page and pep8

--- app_pages/hypothesis_page.py
+++ app_pages/hypothesis_page.py
@@ -4,8 +4,8 @@ def hypothesis_page_body():
     st.title('Hypothesis and Validation')
     st.markdown("1. Healthy leaves do not have any specks on them"
     " therefore they can be detected by the lack of specks.")
-    st.success("The average image plot will show which is most common"
-    " and the standard deviation plots will show the variance in the images")
+    st.success("The average and standard deviation plot shows this. The 
+    " powdery_leaves will have specks on them whereas hhe healthy leaves do not. ")
     st.markdown("2. The model can be trained to a high degree of 95%")
     st.success("Train the model (Convolutional Neural Network(CNN))."
     " The loss and accuracy plots are plotted to show if there is any under"
